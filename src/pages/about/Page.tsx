1adb5ca5112805585aeb07659da73ec509671e7f
Simmone Kelly <simmone.kelly@gmail.com>
2022-12-28 18:08:38 -0500

    adjusting mobile about me

--- src/pages/about/Page.tsx
+++ src/pages/about/Page.tsx
@@ -5,25 +5,25 @@ const AboutPage: React.FC = () => {
   return (
     <div className="main-content_section">
       <h1>About Me</h1>
+      <p>“Who are you?” </p>
       <p>
-        Email marketer turned full stack web developer. When I'm not your guy in
-        the chair, you can find me picking up heavy things and setting them back
-        down in the gym, grabbing a nice craft beer at a local brewery, or
-        living out my best elder emo life singing along with a crowd to
-        nostalgic pop punk.
+        I hardly know, just at present—at least I know who I was when I got up
+        this morning, but I think I must have been changed several times since
+        then.
       </p>
-      <p>Always remember:</p>
-      <p>Never let what matters least get in the way of what matters most.</p>
-
       <div className="aboutMe-columns">
         <div>
-          <h2>Header</h2>
+          <h2>Who Am I?</h2>
           <p>
-            “Who are you?” said the Caterpillar. This was not an encouraging
-            opening for a conversation. Alice replied, rather shyly, “I—I hardly
-            know, Sir, just at present—at least I know who I was when I got up
-            this morning, but I think I must have been changed several times
-            since then
+            Email marketer turned full stack web developer. When I'm not your
+            guy in the chair, you can find me picking up heavy things and
+            setting them back down in the gym, grabbing a nice craft beer at a
+            local brewery, or living out my best elder emo life singing along
+            with a crowd to nostalgic pop punk.
+          </p>
+          <p>Always remember:</p>
+          <p>
+            Never let what matters least get in the way of what matters most.
           </p>
           <p>
             “The best way to explain it is to do it.” —Chapter 3, A Caucus-Race
