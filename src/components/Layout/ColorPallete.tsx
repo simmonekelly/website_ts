73aad3adea9faeccf04e6a37e5b82f3615ad3b6f
Simmone Kelly <simmone.kelly@gmail.com>
2023-05-05 16:38:41 -0400

    changing colors to yellow and adding color pallette

---
+++ src/components/Layout/ColorPallete.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const ColorPallete: React.FC = () => {
+  return (
+    <div style={{ backgroundColor: "#ffffff", height: "100px" }}>
+      <div style={{ backgroundColor: "#FFD131" }}>Main</div>
+      <div style={{ backgroundColor: "#f9dc5c" }}>one</div>
+      <div style={{ backgroundColor: "#fcefb4" }}>two</div>
+      <div style={{ backgroundColor: "#fdf8e1" }}>three</div>
+      white
+    </div>
+  );
+};
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,16 @@
 import React, { PropsWithChildren } from "react";
 import Header from "../Header/Header";
 import "./Layout.css";
+import { ColorPallete } from "./ColorPallete";
 
 const Layout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className="page-layout">
       <Header />
-      <main className="main-content">{children}</main>
+      <main className="main-content">
+        {children}
+        <ColorPallete />
+      </main>
     </div>
   );
 };
