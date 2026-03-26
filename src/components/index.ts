5ada26a79042b49c7935465d7af23a1033ea5415
Simmone Kelly <simmone.kelly@onepeloton.com>
2026-03-26 12:23:15 -0400

    chore: remove dead files replaced by new architecture

--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-import ProjectCard from "./ProjectCard/ProjectCard";
-import SkillCard from "./SkillCard/SkillCard";
-
-export { ProjectCard, SkillCard };
--- src/pages/index.ts
+++
@@ -1,5 +0,0 @@
-import AboutPage from "./about/Page";
-import SkillsPage from "./skills/Page";
-import ProjectsPage from "./projects/Page";
-
-export { AboutPage, SkillsPage, ProjectsPage };
